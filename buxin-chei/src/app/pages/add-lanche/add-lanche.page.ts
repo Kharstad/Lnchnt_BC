e3411243728b1755e05963c0082a9bfe33d54e54
Técnico em Informática 2018.2 <39121742018.2@senacrjedu.local>
2019-10-23 11:17:43 -0300

    23/10 g

---
+++ buxin-chei/src/app/pages/add-lanche/add-lanche.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-lanche',
+  templateUrl: './add-lanche.page.html',
+  styleUrls: ['./add-lanche.page.scss'],
+})
+export class AddLanchePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ buxin-chei/src/app/pages/cadastro-cliente/cadastro-cliente.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro-cliente',
+  templateUrl: './cadastro-cliente.page.html',
+  styleUrls: ['./cadastro-cliente.page.scss'],
+})
+export class CadastroClientePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
