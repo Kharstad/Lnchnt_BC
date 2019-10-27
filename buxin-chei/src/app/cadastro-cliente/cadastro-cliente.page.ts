902a6527311e2460d5d13ab0d9aa16a23a4e6c3b
Enos Fernandes <47147026+Kharstad@users.noreply.github.com>
2019-10-27 13:19:58 -0300

    27/10 E

--- buxin-chei/src/app/cadastro-cliente/cadastro-cliente.page.ts
+++ buxin-chei/src/app/cadastro-cliente/cadastro-cliente.page.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { AlertController, Platform } from '@ionic/angular';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
-
 import {
   GoogleMaps,
   GoogleMap,
@@ -13,6 +12,7 @@ import {
   MyLocation
 } from '@ionic-native/google-maps';
 import { Usuario } from '../model/usuario';
+import { UsuarioService } from '../services/usuario.service';
 
 @Component({
   selector: 'app-cadastro-cliente',
@@ -25,11 +25,134 @@ export class CadastroClientePage implements OnInit {
   protected id: any = null;
   protected posLat: number = 0;
   protected posLng: number = 0;
-  protected map: GoogleMap; 
+  protected map: GoogleMap;
 
-  constructor(){ }
+  constructor(
+    protected usuarioService: UsuarioService,
+    protected alertController: AlertController,
+    protected activatedRoute: ActivatedRoute,
+    protected router: Router,
+    protected geolocation: Geolocation,
+    protected platform: Platform
+  ) { }
 
-  ngOnInit() {
+  async ngOnInit() {
+    // Localização atual
+    this.localAtual();
+    // Plataforma e GoogleMaps
+    await this.platform.ready();
+    await this.loadMap();
+    // Pega Id para autilaização dos dados do Usuário
+    this.id = this.activatedRoute.snapshot.paramMap.get('id');
   }
 
+  onsubmit(form) {
+    this.usuario.lat = this.posLat;
+    this.usuario.lng = this.posLng;
+    if (!this.id) {
+      this.usuarioService.save(this.usuario).then(
+        res => {
+          form.reset();
+          this.usuario = new Usuario;
+          console.log('Cadastrado!');
+          this.presentAlert('Aviso', 'Cadastrado!')
+          this.router.navigate(['/']);
+        },
+        erro => {
+          console.log('Erro: ' + erro);
+          this.presentAlert('Erro', 'Não foi possivel cadastrar!')
+        }
+      )
+    } else {
+      this.usuarioService.update(this.usuario, this.id).then(
+        res => {
+          form.reset();
+          this.usuario = new Usuario;
+          this.presentAlert('Aviso', 'Atualizado!')
+          this.router.navigate(['/tabs/perfilUsuario', this.id]);
+        },
+        erro => {
+          console.log('Erro: ' + erro);
+          this.presentAlert('Erro', 'Não foi possivel atualizar!')
+        }
+      )
+    }
+  }
+
+  localAtual() {
+    this.geolocation.getCurrentPosition().then(
+      resp => {
+        this.posLat = resp.coords.latitude;
+        this.posLng = resp.coords.longitude;
+      }).catch(
+        error => {
+          console.log('Não foi possivel pegar sua localização!', error);
+        });
+  }
+
+  // Alerts-------------------
+  async presentAlert(tipo: string, texto: string) {
+    const alert = await this.alertController.create({
+      header: tipo,
+      // subHeader: 'Subtitle',
+      message: texto,
+      buttons: ['OK']
+    });
+    await alert.present();
+  }
+
+  loadMap() {
+    this.map = GoogleMaps.create('map_canvas', {
+      'camera': {
+        'target': {
+          "lat": this.posLat,
+          "lng": this.posLng
+        },
+        'zoom': 15
+      }
+    });
+    // this.addCluster(this.dummyData());
+    this.minhaLocalizacao();
+  }
+
+  minhaLocalizacao() {
+    LocationService.getMyLocation().then(
+      (myLocation: MyLocation) => {
+        this.map.setOptions({
+          camera: {
+            target: myLocation.latLng
+          }
+        })
+        // Adiciona marcador no Mapa
+        let marker: Marker = this.map.addMarkerSync({
+          position: {
+            lat: myLocation.latLng.lat,
+            lng: myLocation.latLng.lng
+          },
+          icon: '#00ff00',
+          title: 'Titulo',
+          snippet: 'Comentário'
+        })
+        // adicionar eventos no mapa
+        marker.on(GoogleMapsEvent.MARKER_CLICK).subscribe(
+          res => {
+            marker.setTitle(this.usuario.nome)
+            marker.setSnippet(this.usuario.nickname)
+            marker.showInfoWindow()
+          }
+        )
+        // colocar pontos extras
+        this.map.on(GoogleMapsEvent.MAP_CLICK).subscribe(
+          res => {
+            this.map.addMarker({
+              position: {
+                lat: res[0].lat,
+                lng: res[0].lng
+              }
+            })
+          }
+        )
+      }
+    )
+  }
 }
--- buxin-chei/src/app/services/usuario.service.ts
+++ buxin-chei/src/app/services/usuario.service.ts
@@ -1,9 +1,56 @@
 import { Injectable } from '@angular/core';
+import { Usuario } from '../model/usuario';
+import { map } from 'rxjs/operators';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuarioService {
 
-  constructor() { }
+  constructor(
+    protected fire: AngularFirestore,
+    protected afAuth: AngularFireAuth
+  ) { }
+
+  save(usuario) {
+    return this.afAuth.auth.createUserWithEmailAndPassword(usuario.email, usuario.pws)
+      .then(
+        res => {
+          return this.fire.collection('usuario').doc(res.user.uid).set({
+            nome: usuario.nome,
+            nickname: usuario.nickname,
+            email: usuario.email,
+            pws: usuario.pws,
+            foto: usuario.foto,
+            ativo: usuario.ativo,
+            lat: usuario.lat,
+            lng: usuario.lng
+          });
+        }
+      )
+  }
+
+  getAll() {
+    return this.fire.collection('usuario').snapshotChanges()
+      .pipe(
+        map(dados =>
+          dados.map(d => ({ key: d.payload.doc.id, ...d.payload.doc.data() }))
+        )
+      )
+  }
+
+  get(id) {
+    return this.fire.collection('usuario').doc<Usuario>(id).valueChanges();
+  }
+
+  update(usuario: Usuario, id: string) {
+    return this.fire.collection('usuario').doc<Usuario>(id)
+      .update(usuario);
+  }
+
+  remove(usuario: any) {
+    return this.fire.collection('usuarios').doc(usuario.key).delete();
+  }
 }
