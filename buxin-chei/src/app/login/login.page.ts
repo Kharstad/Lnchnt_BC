175fe3ab5c8167a5e0a54c56d6834817bc4f1331
Técnico em Informática 2018.2 <39121742018.2@senacrjedu.local>
2019-10-30 11:05:17 -0300

    30/10 g

--- buxin-chei/src/app/login/login.page.ts
+++ buxin-chei/src/app/login/login.page.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase/app';
+import { AlertController, LoadingController } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { GooglePlus } from '@ionic-native/google-plus/ngx';
+import { Device } from '@ionic-native/device/ngx';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +12,86 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-
-  constructor() { }
+  protected email: string = "";
+  protected pws: string = "";
+  constructor(
+    public afAuth: AngularFireAuth,
+    protected alertController: AlertController,
+    public loadingController: LoadingController,
+    protected router: Router,
+    private googlePlus: GooglePlus,
+    private device: Device,
+  ) { }
 
   ngOnInit() {
   }
+  onSubmit(form) {
+    this.presentLoading();
+    this.login();
+  }
+  login() {
+    this.afAuth.auth.signInWithEmailAndPassword(this.email, this.pws).then(
+      res => {
+        console.log(res.user);
+        this.dismmissLoading();
+      },
+      erro => {
+        console.log("Erro" + erro);
+        this.dismmissLoading();
+        this.presentAlert("Erro", "E-mail ou senha invalidos!");
+      }
+    ).catch(erro => {
+      console.log("Erro no sistema" + erro);
 
+    })
+  }
+  logout() {
+    this.afAuth.auth.signOut();
+  }
+  loginGoogle() {
+    console.log('Device platform is: ' + this.device.platform);
+    if(this.device.platform == "browser"){
+    this.afAuth.auth.signInWithPopup(new auth.GoogleAuthProvider()).then(
+      res => {
+        console.log(res);
+        this.router.navigate(['/'])
+      },
+      erro => {
+        console.log("ERRO: ", erro )
+        this.presentAlert("Erro", "login ou senha invalidos!");
+      }
+    )}else{
+      this.loginGooglePlus();
+    }
+  }
+  loginGooglePlus(){
+    this.googlePlus.login({})
+  .then(res => console.log(res))
+  .catch(err => console.error(err));
+  }
+  async presentAlert(tipo: string, texto: string) {
+    const alert = await this.alertController.create({
+      header: tipo,
+      message: texto,
+      buttons: ['go']
+    });
+
+    await alert.present();
+  }
+  async presentLoading() {
+    const loading = await this.loadingController.create({
+      message: 'carregando...',
+      //duration: 2000
+    }).then(
+      res => {
+        res.present()
+      }
+    );
+  }
+  async dismmissLoading() {
+    await this.loadingController.dismiss();
+
+  }
 }
+
+
