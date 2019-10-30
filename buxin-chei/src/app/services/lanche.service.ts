2ba5c5d6f8e9af9b15ba209b592316fd25fcfe1d
bmellborges <brunomelloborges@outlook.com>
2019-10-30 11:07:09 -0300

    ola

--- buxin-chei/src/app/services/lanche.service.ts
+++ buxin-chei/src/app/services/lanche.service.ts
@@ -1,4 +1,4 @@
-  
+
 import { Injectable } from '@angular/core';
 import { Lanche } from '../model/lanche';
 import { AngularFirestore } from '@angular/fire/firestore';
@@ -8,30 +8,15 @@ import { map } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
-export class PlayerService {
+export class LancheService {
 
   constructor(
     protected fire: AngularFirestore
   ) { }
 
-<<<<<<< HEAD
-=======
-  save(lanche) {
-    return this.fire.collection('lanche')
-      .add({
-        nome: lanche.nome,
-        categoria: lanche.categoria,
-        descricao: lanche.descricao,
-        quant: lanche.quant,
-        valor: lanche.valor,
-        fotos: lanche.fotos,
-        ativo: lanche.ativo,
-      });
-  }
 
   getAll() {
     return this.fire.collection('lanche').snapshotChanges()
->>>>>>> 3e110d7b818572cbe755acf28a3d08337e2b99f4
       .pipe(
         map(dados =>
           dados.map(d => ({ key: d.payload.doc.id, ...d.payload.doc.data() }))
@@ -58,6 +43,7 @@ export class PlayerService {
     return this.fire.collection("lanches").doc<Lanche>(id)
       .update(lanche);
   }
+  
 
-  }
 }
+
